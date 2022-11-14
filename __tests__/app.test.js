938f95def703dd0edd308623a662621c894442d5
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-11-14 15:58:14 +0000

    this will add the app and testing suite to return the game categories

---
+++ __tests__/app.test.js
@@ -0,0 +1,24 @@
+const app = require("../app.js");
+
+const request = require("supertest");
+
+const db = require("../db/connection.js");
+
+const seed = require("../db/seeds/seed.js");
+
+const testData = require("../db/data/test-data/index.js");
+beforeEach(() => {
+  return seed(testData);
+});
+
+describe("/api/categories", () => {
+  test("GET 200 - responds with an array of category objects", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then((res) => {
+        const { games } = res.body;
+        expect(games).toBeInstanceOf(Array);
+      });
+  });
+});
