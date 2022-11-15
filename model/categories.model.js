05b6781b38a18da3a512a5532301173712ee8121
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-11-15 11:59:07 +0000

    removed unneeded console log

--- model/categories.model.js
+++ model/categories.model.js
@@ -2,7 +2,6 @@ const db = require("../db/connection.js");
 
 exports.selectCategories = () => {
   return db.query("SELECT * FROM categories;").then((result) => {
-    console.log(result.rows);
     return result.rows;
   });
 };
