39811cbdc3478b5b7ef68261fed48590c2129731
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-11-15 12:01:13 +0000

    added code to return 'Bad Request' message if path doesn't exist

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const { getCategories } = require("./controllers/categories.controller");
 
 const app = express();
 
-
 app.get("/api/categories", getCategories);
+
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "Bad Request!" });
+});
 
 module.exports = app;
