0003ecfb84e764fb3525ecbe6f86a74725e4ec32
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-11-15 10:31:30 +0000

    changed controller and model to more specific names instead of having huge files

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,4 +1,4 @@
-const { selectCategories } = require("../model/games.model.js");
+const { selectCategories } = require("../model/categories.model.js");
 
 exports.getCategories = (req, res) => {
   selectCategories().then(( categories ) => {
