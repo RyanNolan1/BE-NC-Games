1b4e4f7d00359e4cb2238d08f9a1ffbcef32ae0e
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-11-16 11:35:27 +0000

    this will add the app get and require reviews controller to the app

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express");
 
 const { getCategories } = require("./controllers/categories.controller");
 
+const { getReviews } = require("./controllers/reviews.controller");
+
 const app = express();
 
 app.get("/api/categories", getCategories);
+
+app.get("/api/reviews", getReviews);
 
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "Bad Request!" });
