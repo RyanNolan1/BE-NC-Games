044c2ba0e96a397c8723cd66fb8756fa8f41d712
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-11-17 13:49:45 +0000

    this will return a 400 status code if the request is the wrong data type and also a 500 status code and message if there is a server error

--- app.js
+++ app.js
@@ -14,6 +14,26 @@ app.get("/api/reviews", getReviews);
 
 app.get("/api/reviews/:review_id", getReviewById);
 
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request!" });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "SERVER ERROR" });
+});
+
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "Bad Request!" });
 });
