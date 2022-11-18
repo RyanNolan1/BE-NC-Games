b834c9fdca34c462da51083407503fc227912bae
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-11-18 14:06:28 +0000

    this willl change the model so that it only runs if the review id exists

--- model/comments-by-review-id.model.js
+++ model/comments-by-review-id.model.js
@@ -1,18 +1,15 @@
 const db = require("../db/connection.js");
+const { checkReviewExists } = require("../utils/db.js");
 
 exports.selectReviewCommentsById = (review_id) => {
-  return db
-    .query(
-      `SELECT comment_id, votes, created_at, author, body, review_id FROM comments WHERE review_id = $1 ORDER BY created_at DESC;`,
-      [review_id]
-    )
+  return checkReviewExists(review_id)
+    .then(() => {
+      return db.query(
+        `SELECT comment_id, votes, created_at, author, body, review_id FROM comments WHERE review_id = $1 ORDER BY created_at DESC;`,
+        [review_id]
+      );
+    })
     .then((res) => {
-      if (res.rows.length === 0) {
-        return Promise.reject({
-          status: 200,
-          msg: [],
-        });
-      }
       return res.rows;
     });
 };
