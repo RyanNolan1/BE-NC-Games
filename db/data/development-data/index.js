748c78b0f613fe9716d8a4d23c56a351ec5a71f9
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-11-24 09:48:21 +0000

    this will remove an unneeded object

--- db/data/development-data/index.js
+++ db/data/development-data/index.js
@@ -1,5 +1,3 @@
-export { categoryData, commentData, reviewData, userData };
-
 exports.categoryData = require("./categories.js");
 exports.commentData = require("./comments.js");
 exports.reviewData = require("./reviews.js");
