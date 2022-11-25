dba39039c0935ac538317b9cd2b95ad6e3f78b74
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-11-25 08:50:23 +0000

    not sure what's changed

--- controllers/post-comment.controller.js
+++ controllers/post-comment.controller.js
@@ -3,10 +3,11 @@ const { insertComment } = require("../model/post-comment.model");
 exports.postComment = (req, res, next) => {
   const newComment = req.body;
   const { review_id } = req.params;
-  insertComment(newComment, review_id).then((insertedComment) => {
-    res.status(201).send({ comment: insertedComment });
-})
-  .catch((err) => {
-    next((err))
-  });
+  insertComment(newComment, review_id)
+    .then((insertedComment) => {
+      res.status(201).send({ comment: insertedComment });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
