a5868126b3a464d3d8f7b2c7b6abec0a225efe44
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-12-05 13:03:11 +0000

    this will fix the issue with nested tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -341,17 +341,15 @@ describe("/api/users", () => {
             name: expect.any(String),
             avatar_url: expect.any(String),
           });
-
-          test("GET: 404 - responds with an error if path doesn't exist", () => {
-            return request(app)
-              .get("/api/nonsense")
-              .expect(404)
-              .then((res) => {
-                expect(res.body.msg).toBe("Bad Request!");
-              });
-            });
-          });
         });
-    });
+      });
+  });
+  test("GET: 404 - responds with an error if path doesn't exist", () => {
+    return request(app)
+      .get("/api/nonsense")
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Bad Request!");
+      });
   });
 });
